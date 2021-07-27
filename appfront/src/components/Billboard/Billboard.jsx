1e8b86c86fe6a2c0f0b27c07fb1156d5743eb628
Jack Praveen Raj Ilango <jackpraveenraj.work@gmail.com>
2021-07-27 08:57:14 +0000

    commit on 'main'

---
+++ appfront/src/components/Billboard/Billboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from "./Billboard.module.css" 
+
+const Billboard = () => {
+    return (
+        <div className={styles.Billboard}>
+            Navigation
+        </div>
+    );
+
+};
+
+export default Billboard;
